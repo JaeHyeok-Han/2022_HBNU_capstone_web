ffaa7393106b3b790e06dcee6c09edbd43712cb0
JaeHyeok-Han <hjh0633@gmail.com>
2022-11-16 13:46:29 +0900

    Feat: 스타일드 컴포넌트 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,25 @@
 import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
+import styled from 'styled-components';
 import MainPage from './pages/MainPage';
 import SearchPage from './pages/SearchPage';
 import DetailPage from './pages/DetailPage';
 
+const Container = styled.div`
+  min-width: 100vw;
+  min-height: 100vh;
+`;
+
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Navigate to={'/main'} replace={true} />} />
-      <Route path="/main" element={<MainPage />} />
-      <Route path="/search" element={<SearchPage />} />
-      <Route path="/detail" element={<DetailPage />} />
-    </Routes>
+    <Container>
+      <Routes>
+        <Route path="/" element={<Navigate to={'/main'} replace={true} />} />
+        <Route path="/main" element={<MainPage />} />
+        <Route path="/search" element={<SearchPage />} />
+        <Route path="/detail" element={<DetailPage />} />
+      </Routes>
+    </Container>
   );
 }
 
